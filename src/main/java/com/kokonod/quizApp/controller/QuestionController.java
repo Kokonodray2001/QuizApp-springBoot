7ff83a1ef9068419b8ad1750bf13b11a014674dc
Kokonod Ray <kokonodray2001@gmail.com>
2024-05-10 23:13:42 +0530

    updated all CRUD operation

--- src/main/java/com/kokonod/quizApp/controller/QuestionController.java
+++ src/main/java/com/kokonod/quizApp/controller/QuestionController.java
@@ -27,4 +27,14 @@ public class QuestionController {
     public Question addQuestion(@RequestBody Question question){
         return  questionService.addQuestion(question);
     }
+
+    @PutMapping("updateQuestion/{id}")
+    public String updateQuestion(@RequestBody Question question , @PathVariable String id){
+        return questionService.updateQuestion(question,id);
+    }
+
+    @DeleteMapping("deleteQuestion/{id}")
+    public String deleteQuestion(@PathVariable String id){
+        return questionService.deleteQuestion(id);
+    }
 }
--- src/main/java/com/kokonod/quizApp/service/QuestionService.java
+++ src/main/java/com/kokonod/quizApp/service/QuestionService.java
@@ -2,10 +2,12 @@ package com.kokonod.quizApp.service;
 
 import com.kokonod.quizApp.dao.QuestionDao;
 import com.kokonod.quizApp.model.Question;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuestionService {
@@ -23,4 +25,39 @@ public class QuestionService {
         return  questionDao.save(question);
 
     }
+
+    public String updateQuestion(Question question, String id) {
+            Optional<Question> optionalQuestion = questionDao.findById(Integer.parseInt(id));
+            if(optionalQuestion.isPresent()){
+                Question existingQuestion =  optionalQuestion.get();
+                if(question.getQuestionTitle() != null)
+                    existingQuestion.setQuestionTitle(question.getQuestionTitle());
+                if(question.getCategory() != null)
+                    existingQuestion.setCategory(question.getCategory());
+
+                if(question.getOption1() != null)
+                    existingQuestion.setOption1(question.getOption1());
+                if(question.getOption2() != null)
+                    existingQuestion.setOption2(question.getOption2());
+                if(question.getOption3() != null)
+                    existingQuestion.setOption3(question.getOption3());
+                if(question.getOption4() != null)
+                    existingQuestion.setOption4(question.getOption4());
+
+                if(question.getDifficultyLevel()!=null)
+                    existingQuestion.setDifficultyLevel(question.getDifficultyLevel());
+                if(question.getRightAnswer()!=null)
+                    existingQuestion.setRightAnswer(question.getRightAnswer());
+
+                questionDao.save(existingQuestion);
+            }
+
+         return "updated sucess";
+    }
+
+    public String deleteQuestion(String id) {
+        questionDao.deleteById(Integer.parseInt(id));
+        return "deleted record with id " + id;
+    }
+
 }
